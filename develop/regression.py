22e2062347f8538b12c673b685e4c011d83a1cbb
mcc <andi.m.mcclure@gmail.com>
2017-08-03 02:34:13 -0400

    regression.py add scaffolding to support arbitrary run modes (for compile tests)

--- develop/regression.py
+++ develop/regression.py
@@ -28,6 +28,8 @@
 #   # Omit file
 #       Invoke interpreter WITHOUT test file as argument
 #
+#   # Tags: SOMETHING SOMETHING
+#       Special tags (runner-specific, may impact whether runs)
 
 # Usage: ./develop/regression.py -a
 # Tested with Python 2.6.1
@@ -45,28 +47,28 @@ def projectRelative( filename ):
 prjroot = os.path.join( os.path.dirname(__file__), ".." )
 stddir  = "test"
 stdfile = "test/regression.txt"
-badfile = "test/regression-known-bad.txt"
+runnername = "default"
 
 help  = "%prog -a\n"
 help += "\n"
 help += "Accepted arguments:\n"
 help += "-f [filename.em]  # Check single file\n"
 help += "-t [filename.txt] # Check all paths listed in file\n"
-help += "-r [path]         # Set the project root\n"
+help += "--root [path]     # Set the project root\n"
+help += "-r          # Runner (default, meta, cs, cpp, js, all)\n"
 help += "-a          # Check all paths listed in standard " + stdfile + "\n"
-help += "-A          # Also check paths listed in " + badfile + "\n"
+help += "-A          # Run tests even if they are known bad\n"
 help += "-v          # Print all output\n"
-help += "-i [path]   # Use custom emily script\n"
-help += "-p [path]   # Use custom Python\n"
+help += "-i [path]   # Use custom emily script (when running Emily)\n"
+help += "-p [path]   # Use custom Python (when running Python)\n"
 help += "--p3        # Use python3\n"
-help += "--meta      # Run through self-hosted interpreter\n"
 help += "--md        # \"Meta depth\" (nest how many interpreters?)\n"
 help += "--untested  # Check repo hygiene-- list tests in sample/test not tested"
 
 parser = optparse.OptionParser(usage=help)
 for a in ["a", "A", "v", "-p3", "-meta", "-untested"]: # Single letter args, flags
     parser.add_option("-"+a, action="store_true")
-for a in ["f", "t", "r", "i", "p", "-md"]: # Long args with arguments
+for a in ["f", "t", "r", "i", "p", "-root", "-md"]: # Long args with arguments
     parser.add_option("-"+a, action="append")
 
 (options, cmds) = parser.parse_args()
@@ -82,14 +84,17 @@ if cmds:
 indices = []
 files = []
 
-if flag("r"):
-    prjroot = flag("r")[0]
+if flag("root"):
+    prjroot = flag("root")[0]
 
 if flag("a") or flag("A"):
     indices += [projectRelative(stdfile)]
 
 if flag("A"):
     indices += [projectRelative(badfile)]
+
+if flag("r"):
+    runnername = flag("runner")[0]
 
 indices += flag("t")
 
@@ -136,7 +141,8 @@ stdmetalist = [stdmeta] if flag("meta") or flag("md") else []
 if flag("md"):
     stdmetalist *= int(flag("md")[0])
 
-stdcall = [stdpython, stdscript] + stdmetalist
+stdcall = [stdpython, stdscript]
+stdmeta = stdcall + stdmetalist
 
 expectp = re.compile(r'# Expect(\s*failure)?(\:?)', re.I)
 linep = re.compile(r'# ?(.+)$', re.S)
@@ -157,98 +163,205 @@ failures = 0
 # So make a copy before any potential modification occurs.
 globalEnv = copy.deepcopy( os.environ )
 
-for filename in files:
-    expectfail = False
-    scanning = False
-    outlines = ''
-    env = None
-    args = []
-    omit = False
-    earlyfail = False
+# This is not the best way to use OO; only a bit of state persists in the object,
+# and the object "resets" itself each time you run a file.
+class BaseRunner:
+    def __init__(s):
+        s.failures = 0
+        s.trials = 0
 
-    # Pre-scan the file for magic comments with test instructions
-    with open(filename) as f:
-        for line in f.readlines():
-            # First determine if this is an expect directive
-            expect = expectp.match(line) # Expect:
-            inline = inline_expectp.match(line)
-            # If the inline pattern matches and the inline body isn't empty,
-            # then we're looking at an inline expect directive
-            if inline and not inline.group(2).isspace():
-                if inline.group(1) and not inline.group(1).isspace(): # "Expect file"
-                    outlines += open(inline.group(2).strip()).read().rstrip()
-                else:
-                    outlines += inline.group(2)
-            # Otherwise, if it's an expect we're beginning a multiline expect
-            elif expect:
-                expectfail = bool(expect.group(1))
-                scanning = bool(expect.group(2))
-            else:
-                if scanning: # If currently inside an expect block
-                    outline = linep.match(line)
-                    if outline:
-                        outlines += outline.group(1)
+    def phases(s):
+        return 1
+
+    def should(s):
+        for x in s.tags:
+            for y in s.name():
+                if x == "no-" + y:
+                    return False
+        return True
+
+    def phasename(s, phase):
+        return "Running"
+
+    def phaseenv(s, phase):
+        return s.env
+
+    def normalargs(s):
+        return s.args+([] if s.omit else [s.filename])
+
+    def phaseinvoke(s, phase): # Subclasses must reimplement
+        raise RuntimeError()
+
+    def phaserun(s, phase):
+        env = s.phaseenv(phase)
+        invoke = s.phaseinvoke(phase)
+
+        print "%s %s..." % (s.phasename(phase), s.filename)
+        try:
+            proc = subprocess.Popen(invoke, stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env if env else globalEnv)
+        except OSError as e:
+            print "\nCATASTROPHIC FAILURE: Couldn't find emily?:"
+            print e
+            print "Make sure you ran a plain `make` first."
+            sys.exit(1)
+        result = proc.wait()
+        outstr, errstr = proc.communicate()
+
+        result = bool(result)
+        outstr = outstr.rstrip()
+        errstr = errstr.rstrip()
+
+        if result ^ s.expectfail:
+            print "\tFAIL:   Process failure " + ("expected" if s.expectfail else "not expected") + " but " + ("seen" if result else "not seen")
+            if errstr:
+                print "\n"+pretag("STDERR",errstr)
+            return False
+        elif outstr != s.outlines:
+            print "\tFAIL:   Output differs"
+            print "\n%s\n\n%s" % ( pretag("EXPECT", s.outlines), pretag("STDOUT", outstr) )
+            return False
+        elif flag("v"):
+            if outstr:
+                print pretag("STDOUT", outstr)
+            if outstr and errstr:
+                print
+            if errstr:
+                print pretag("STDERR",errstr)
+        return True
+
+    def run(s, filename):
+        # Reset object state
+        s.filename = filename
+        s.expectfail = False
+        s.env = None
+        s.args = []
+        s.omit = False
+        s.outlines = ''
+        s.tags = []
+        s.trials += 1
+
+        scanning = False
+        earlyfail = False
+        
+        # Pre-scan the file for magic comments with test instructions
+        with open(filename) as f:
+            for line in f.readlines():
+                # First determine if this is an expect directive
+                expect = expectp.match(line) # Expect:
+                inline = inline_expectp.match(line)
+                # If the inline pattern matches and the inline body isn't empty,
+                # then we're looking at an inline expect directive
+                if inline and not inline.group(2).isspace():
+                    if inline.group(1) and not inline.group(1).isspace(): # "Expect file"
+                        s.outlines += open(inline.group(2).strip()).read().rstrip()
                     else:
-                        scanning = False
+                        s.outlines += inline.group(2)
+                # Otherwise, if it's an expect we're beginning a multiline expect
+                elif expect:
+                    s.expectfail = bool(expect.group(1))
+                    scanning = bool(expect.group(2))
+                else:
+                    if scanning: # If currently inside an expect block
+                        outline = linep.match(line)
+                        if outline:
+                            s.outlines += outline.group(1)
+                        else:
+                            scanning = False
 
-                # Only an expect directive can end an expect block
-                if not scanning: # Other directives:
-                    argline = argp.match(line) # Arg:
-                    if argline:
-                        args += [argline.group(1)]
+                    # Only an expect directive can end an expect block
+                    if not scanning: # Other directives:
+                        argline = argp.match(line) # Arg:
+                        if argline:
+                            s.args += [argline.group(1)]
 
-                    envline = envp.match(line) # Env:
-                    if envline:
-                        if not env:
-                            env = copy.deepcopy( globalEnv )
-                        kvline = kvp.match( envline.group(1) )
-                        if not kvline:
-                            print "\tMALFORMED TEST: \"Env:\" line not of form KEY=VALUE"
-                            earlyfail = True
-                            break
-                        env[kvline.group(1)] = kvline.group(2)
+                        envline = envp.match(line) # Env:
+                        if envline:
+                            if not s.env:
+                                s.env = copy.deepcopy( globalEnv )
+                            kvline = kvp.match( envline.group(1) )
+                            if not kvline:
+                                print "\tMALFORMED TEST: \"Env:\" line not of form KEY=VALUE"
+                                earlyfail = True
+                                break
+                            env[kvline.group(1)] = kvline.group(2)
 
-                    if omitp.match(line):
-                        omit = True
+                        if omitp.match(line):
+                            omit = True
 
-    if earlyfail:
-        failures += 1
-        continue
+        if earlyfail:
+            s.failures += 1
+            return False
 
-    print "Running %s..." % (filename)
-    try:
-        proc = subprocess.Popen(stdcall+args+([] if omit else [filename]), stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env if env else globalEnv)
-    except OSError as e:
-        print "\nCATASTROPHIC FAILURE: Couldn't find emily?:"
-        print e
-        print "Make sure you ran a plain `make` first."
-        sys.exit(1)
-    result = proc.wait()
-    outstr, errstr = proc.communicate()
+        if s.should():
+            s.outlines = s.outlines.rstrip()
+            s.expectfail = bool(s.expectfail)
 
-    result = bool(result)
-    expectfail = bool(expectfail)
-    outlines = outlines.rstrip()
-    outstr = outstr.rstrip()
-    errstr = errstr.rstrip()
+            for phase in range(s.phases()):
+                if not s.phaserun(phase):
+                    s.failures += 1
+                    break
 
-    if result ^ expectfail:
-        print "\tFAIL:   Process failure " + ("expected" if expectfail else "not expected") + " but " + ("seen" if result else "not seen")
-        if errstr:
-            print "\n"+pretag("STDERR",errstr)
-        failures += 1
-    elif outstr != outlines:
-        print "\tFAIL:   Output differs"
-        print "\n%s\n\n%s" % ( pretag("EXPECT", outlines), pretag("STDOUT", outstr) )
-        failures += 1
-    elif flag("v"):
-        if outstr:
-            print pretag("STDOUT", outstr)
-        if outstr and errstr:
-            print
-        if errstr:
-            print pretag("STDERR",errstr)
+        return True
 
-print "\n%d tests failed of %d" % (failures, len(files))
+    def runall(s):
+        for filename in files:
+            s.run(filename)
+
+        print "\n%d tests failed of %d" % (s.failures, s.trials)
+
+class NormalRunner(BaseRunner):
+    def name(s):
+        return ["default"]
+
+    def phaseinvoke(s, phase):
+        return stdcall + s.normalargs()
+
+class MetaRunner(BaseRunner):
+    def name(s):
+        return ["meta"]
+
+    def phaseinvoke(s, phase):
+        return stdmeta + s.normalargs()
+
+class CsRunner(BaseRunner):
+    def name(s):
+        return ["compiler", "cs"]
+
+class CppRunner(BaseRunner):
+    def name(s):
+        return ["compiler", "cs"]
+
+class JsRunner(BaseRunner):
+    def name(s):
+        return ["compiler", "cs"]
+
+runners = {
+    "default" : NormalRunner(),
+    "meta" : MetaRunner(),
+    "cs" : CsRunner(),
+    "cpp" : CppRunner(),
+    "js" : JsRunner(),
+}
+
+failures = 0
+trials = 0
+
+if runnername == "all":
+    for runnername in runners:
+        runner = runners[runnername]
+        print("Running: " + runnername)
+        
+        runner.runall()
+        failures += runner.failures
+        trials += runner.trials
+
+    print "\nTotal: %d tests failed of %d" % (failures, trials)
+else:
+    if runnername in runners:
+        runner = runners[runnername]
+        runner.runall()
+        failures = runner.failures
+    else:
+        parser.error("Unrecognized runner name " + runnername)
 
 sys.exit(0 if failures == 0 else 1)
